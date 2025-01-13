e84fda8d4a8e90b2b639125d2df93aa6b9d5b2d8
ohprettyhak <me@haklee.me>
2025-01-13 10:56:06 +0900

    feat: add header navigation area

--- apps/web/components/Layout/Header/index.tsx
+++ apps/web/components/Layout/Header/index.tsx
@@ -5,9 +5,18 @@ import * as styles from './styles.css';
 const Header: FC = (): ReactElement => {
   return (
     <header className={styles.root}>
-      <div></div>
-      <nav className={styles.nav}></nav>
-      <div></div>
+      <div className={styles.branding}>Branding</div>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <a href="#">탐색</a>
+          </li>
+          <li>
+            <a href="#">랭킹</a>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.user}>User</div>
     </header>
   );
 };
--- apps/web/components/Layout/Header/styles.css.ts
+++ apps/web/components/Layout/Header/styles.css.ts
@@ -1,13 +1,33 @@
 import { rem, theme } from '@jandies/style';
 import { breakpoint } from '@jandies/style/dist/responsive.css';
-import { style } from '@vanilla-extract/css';
+import { globalStyle, style } from '@vanilla-extract/css';
 
 export const root = style({
-  ...theme.layouts.centerY,
+  ...theme.layouts.center,
+  position: 'fixed',
+  top: 0,
+  left: 0,
   width: '100%',
   height: theme.sizes.headerMobile,
+  paddingInline: theme.sizes.appInline,
+  gap: rem(26),
 
   ...breakpoint({ tablet: { height: theme.sizes.header } }),
+});
+
+export const branding = style({
+  position: 'static',
+  top: 'auto',
+  left: theme.sizes.appInline,
+  transform: 'none',
+
+  ...breakpoint({
+    tablet: {
+      position: 'absolute',
+      top: '50%',
+      transform: 'translateY(-50%)',
+    },
+  }),
 });
 
 export const nav = style({
@@ -15,5 +35,23 @@ export const nav = style({
   width: '100%',
   maxWidth: theme.sizes.appWidth,
   height: '100%',
-  gap: rem(12),
+  paddingInline: 0,
+  marginInline: 'auto',
+
+  ...breakpoint({ tablet: { paddingInline: theme.sizes.appInline } }),
+});
+
+globalStyle(`${nav} ul`, {
+  ...theme.layouts.centerY,
+  listStyle: 'none',
+  gap: rem(13),
+});
+
+export const user = style({
+  ...theme.layouts.centerY,
+  position: 'absolute',
+  top: '50%',
+  right: theme.sizes.appInline,
+  transform: 'translateY(-50%)',
+  gap: rem(13),
 });
