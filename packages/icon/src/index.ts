4f9c4383e9fb9f985385ca90692592c67828d0ac
ohprettyhak <me@haklee.me>
2025-01-13 12:15:15 +0900

    feat: add icon package

---
+++ packages/icon/src/index.ts
@@ -0,0 +1,6 @@
+export * from './components/BrandingLogo';
+export * from './components/CardsStarIcon';
+export * from './components/ExploreIcon';
+export * from './components/SearchIcon';
+export * from './components/NotificationsIcon';
+export * from './components/NotificationsUnreadIcon';
